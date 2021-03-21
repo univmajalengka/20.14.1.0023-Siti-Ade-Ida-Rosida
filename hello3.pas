2ff008fb66d36859f1ec6d2ccc5bacfe963e6f17
SitiAdeIda <adeida060@gmail.com>
2021-03-21 14:51:46 +0700

    Tugas 1

---
+++ hello3.pas
@@ -0,0 +1,16 @@
+{Nama : Siti Ade Ida Rosida}
+{NPM : 201410023}
+{Nama Program : hello3.pas}
+
+program hello3;
+uses crt;
+
+var
+nama : string;
+
+begin
+writeln('Contoh membaca dan menulis inputan');
+writeln('Masukan Nama :');
+readln(nama);
+
+end.
